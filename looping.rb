544c5355a6b8df3e14485fa062ac78f2a1e57cfc
MwirigiBrianBob21 <brian.mwirigi@students.moringaschool.com>
2022-10-17 23:23:56 +0300

    Complete assignment

--- looping.rb
+++ looping.rb
@@ -1,5 +1,11 @@
 def happy_new_year
   # your code here
+  i = 11
+  until i == 1
+    puts i -=1
+    
+  end
+  puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +23,30 @@ end
 
 def fizzbuzz_printer
   # your code here
+  (1..100).each do | num |
+    puts fizzbuzz(num)
+  end
 end
 
 def reverse_string(str)
-  # your code here
+#   # your code here
+#   reversed_string = ""
+#   str.length.times do |i|
+#     reversed_string = str[i] + reversed_string
+
+ 
+  empty = []
+  arr = str.split('')
+  limit = arr.length
+  limit.times{ |i|
+    x = arr.shift
+    empty.insert(0, x)
+  }
+  empty.join
 end
+
+
+   
+#   end
+#   puts reversed_string
+# end
